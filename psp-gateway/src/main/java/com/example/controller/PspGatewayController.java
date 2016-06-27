cff499418fe7ad10d98153332aa7b441abdeba27
victor.andevert <victor.andevert@gmail.com>
2016-06-27 18:20:20 +0200

    update pom dependenies

--- psp-gateway/src/main/java/com/example/controller/PspGatewayController.java
+++ psp-gateway/src/main/java/com/example/controller/PspGatewayController.java
@@ -1,16 +1,9 @@
 package com.example.controller;
 
-import java.net.InetAddress;
-import java.net.URI;
-import java.net.UnknownHostException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
